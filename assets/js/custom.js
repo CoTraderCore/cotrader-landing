b93c60ba118fabbfd81f2ac739f45f4f4abbdef0
kashif R <you@example.com>
2021-04-02 12:42:19 +0500

    roadmap fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,6 +26,7 @@ $navbara.click(function(e){
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
     spaceBetween: 30,
+    loop: true,
     centeredSlides: true,
     pagination: {
       el: '.swiper-pagination',
